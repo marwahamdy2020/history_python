552f9141ffe2cee4bf1acb03edfc0d67cca61001
marwa hamdy <marwa.hamdy.aboelkhair@gmail.com>
2020-10-19 17:10:57 +0200

    config folder import

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 # import flask and import modules/extensions
+
 from flask import Flask
 from dotenv import load_dotenv
-import config
-#from app.models import db
 import os
+from config.settings import settings
+from app.models import db
 
 
 # ceate app object
@@ -13,7 +14,7 @@ app = Flask(__name__)
 APP_ROOT = os.path.join(os.path.dirname(__file__), "..")
 dotenv_path = os.path.join(APP_ROOT, ".env")
 load_dotenv(dotenv_path)
-app.config.from_object('config.settings.' + os.environ.get['ENV'])
+#app.config.from_object('config.settings.' + os.environ.get['ENV'])
 
 # data base
 # db.create_all()
